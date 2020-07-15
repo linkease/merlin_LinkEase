6e5a188c3f7b724e76f53c02bd50baa98f2562d7
janson <janson@gmail.com>
2020-07-15 18:40:10 +0800

    update new binaries

--- config.json.js
+++ config.json.js
@@ -1,7 +1,7 @@
 {
     "description": "易有云2 （LinkEase） 手机相片自动备份，跨平台文件同步，双向同步！", 
     "home_url": "Module_linkease.asp", 
-    "md5": "a35484033ab1f09d288a062b07a93326", 
+    "md5": "aa18e74120132ed644e7f7cdf80a135b", 
     "module": "linkease", 
     "title": "易有云2.0", 
     "version": "2.0.2"
