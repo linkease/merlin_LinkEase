6f35cf23bea1056c5760e9e241084eaeb56eb6cb
janson <janson@gmail.com>
2020-08-19 22:35:53 +0800

    remove image supported in router version

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "易有云2 （LinkEase） 手机相片自动备份，跨平台文件同步，双向同步！", 
     "home_url": "Module_linkease.asp", 
-    "md5": "6d56a5bc153d2ab96a32682fb5669585", 
+    "md5": "ae383aa0df025c0a0193332e96e3e69f", 
     "module": "linkease", 
     "title": "易有云2.0", 
-    "version": "2.0.5"
+    "version": "2.0.7"
 }
