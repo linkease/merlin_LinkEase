4f3f12c8c0d9009a3e5807b23749cf498274c42c
janson <janson@gmail.com>
2020-08-29 20:13:45 +0800

    upgrade to 0.2.96

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "易有云2 （LinkEase） 手机相片自动备份，跨平台文件同步，双向同步！", 
     "home_url": "Module_linkease.asp", 
-    "md5": "ae383aa0df025c0a0193332e96e3e69f", 
+    "md5": "3bb1dcf5a539c66ad6abd1b7dbd0fa26", 
     "module": "linkease", 
     "title": "易有云2.0", 
-    "version": "2.0.8"
+    "version": "2.0.9"
 }
