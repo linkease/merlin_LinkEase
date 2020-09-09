aead70ffc8275e843afa2dfc46078c2240845f64
janson <janson@gmail.com>
2020-09-09 11:50:26 +0800

    v0.2.99

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "易有云2 （LinkEase） 手机相片自动备份，远程管文件，看视频！", 
     "home_url": "Module_linkease.asp", 
-    "md5": "b25af841bff031d415e5d5447d44ed82", 
+    "md5": "d06cd7fb4ab779c9d74cea1a39ad9291", 
     "module": "linkease", 
     "title": "易有云2.0", 
-    "version": "2.9.0"
+    "version": "2.9.1"
 }
