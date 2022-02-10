a5ea942aee27056f28d34954681cfc688979a82a
janson <janson@gmail.com>
2022-02-10 16:17:11 +0800

    upgrade new binary 0.6.9

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "易有云2 （LinkEase） 手机相片自动备份，远程管文件，看视频！", 
     "home_url": "Module_linkease.asp", 
-    "md5": "d22c2b95d305f344a16e94999a313fce", 
+    "md5": "baadbdb90b9cb0afcbaf3c6757250756", 
     "module": "linkease", 
     "title": "易有云2.0", 
-    "version": "2.9.6"
+    "version": "2.9.7"
 }
