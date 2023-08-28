03ca661d3105e23b78a76dd1dc665c693e04a202
janson <janson@gmail.com>
2023-08-28 19:36:34 +0800

    bump to 2.12.5

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "易有云2 （LinkEase） 手机相片自动备份，远程管文件，看视频！", 
     "home_url": "Module_linkease.asp", 
-    "md5": "1b93032e5f87ea535d6c8f3fd27f8f25", 
+    "md5": "51cd53e806cb7151b1f7023d3bdb1ae8", 
     "module": "linkease", 
     "title": "易有云2.0", 
-    "version": "2.12.3"
+    "version": "2.12.5"
 }
